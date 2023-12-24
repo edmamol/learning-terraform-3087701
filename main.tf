bb5dba29a62123c40baab7bd6d8ebc01af404a2c
edmamol <edmamol@yahoo.co.uk>
2023-12-23 22:29:15 -0500

    corrected errors

--- main.tf
+++ main.tf
@@ -14,10 +14,12 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" (
-  default = true
+data "aws_vpc" "default" {
+   default = true
+}
+ 
 
-)
+
 
 
 resource "aws_instance" "blog" {
@@ -44,7 +46,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
-  cidr_blocks = [0.0.0.0/0]
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
@@ -55,7 +57,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
-  cidr_blocks = [0.0.0.0/0]
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
@@ -65,7 +67,7 @@ resource "aws_security_group_rule" "blog_everything_out" {
   from_port   = 0
   to_port     = 0
   protocol    = -1
-  cidr_blocks = [0.0.0.0/0]
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
