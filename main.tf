d99007b815ba11d7dc241467a54231b814bd3a5e
edmamol <edmamol@yahoo.co.uk>
2023-12-23 23:35:42 -0500

    used modules for security groups

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "Learning Terraform"
@@ -39,6 +39,22 @@ resource "aws_security_group" "blog" {
   description = "allow http and https in, Allow everthing else out" 
 
   vpc_id      = data.aws_vpc.default.id
+}
+
+
+module "blog_sg" {
+  
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name    = "blog_new" 
+
+  vpc_id = data.aws_vpc.default.id
+
+  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr  =  ["0.0.0.0/0"]
+
+  egress_rules  =  ["all-all"] 
+
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
