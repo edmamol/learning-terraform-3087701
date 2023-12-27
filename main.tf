33f6d36f7dc330c569a4a83cd5474f0a4fc3830c
edmamol <edmamol@yahoo.co.uk>
2023-12-27 02:13:18 -0500

    instance Id

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ module "alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id         = blog_vpc.target_id
+      target_id         = aws_instance.blog.id
     }
 
   listeners = {
